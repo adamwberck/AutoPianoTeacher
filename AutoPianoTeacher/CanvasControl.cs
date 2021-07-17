8c8f0849654c8503005f01cb0b3dd9a3e351e52d
Adam W Berck <adamwberck@gmail.com>
2021-07-17 14:48:49 -0700

    Add project files.

---
+++ AutoPianoTeacher/CanvasControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoPianoTeacher
+{
+    public class CanvasControl : Control
+    {
+        public CanvasControl() { }
+        protected override void OnPaint(PaintEventArgs e) 
+        {
+            base.OnPaint(e);
+            e.Graphics.FillRectangle(new SolidBrush(ForeColor),new Rectangle(new Point(2,2) ,new Size(1000,400)));
+        }
+    }
+}
